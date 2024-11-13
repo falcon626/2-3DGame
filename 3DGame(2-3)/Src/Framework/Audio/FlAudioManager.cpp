257033a445f59f6f0aab7cd1b8f22cb72eacd148
falcon626 <626hayato@gmail.com>
2024-11-13 10:45:05 +0900

    一旦Audio完成

--- 3DGame(2-3)/Src/Framework/Audio/FlAudioManager.cpp
+++ 3DGame(2-3)/Src/Framework/Audio/FlAudioManager.cpp
@@ -1,41 +1,219 @@
 ﻿#include "FlAudioManager.h"
 
-std::shared_ptr<FMOD::Sound> FlAudioManager::LoadSound(const std::string& filePath)
+const std::shared_ptr<FMOD::Sound> FlAudioManager::LoadSound(const std::string& filePath)
 {
 	// サウンドが既にキャッシュに存在する場合は、それを返す
-	if (m_soundCache.find(filePath) != m_soundCache.end()) {
-		return m_soundCache[filePath];
-	}
+	if (m_soundController.find(filePath) != m_soundController.end()) return m_soundController[filePath].first;
 
 	// サウンドが未ロードの場合、新しくロードする
-	std::unique_ptr<FMOD::Sound, FMODSoundDeleter> newSound;
-	FMOD::Sound* rawSound = nullptr;
-	FMOD_RESULT result = m_upSystem->createSound(filePath.c_str(), FMOD_DEFAULT, nullptr, &rawSound);
-	if (result != FMOD_OK) {
-		throw std::runtime_error("Failed to load sound: " + filePath);
+	auto rawSound{ static_cast<FMOD::Sound*>(nullptr) };
+	if (m_upSystem->createSound(filePath.c_str(), FMOD_DEFAULT, nullptr, &rawSound) != FMOD_OK)
+	{
+		_ASSERT_EXPR(false, "Failed To Create Sound");
+		return nullptr;
 	}
 
-	// 生ポインタを unique_ptr に変換
-	newSound.reset(rawSound);
+	// カスタムデリータを使った unique_ptr を作成
+	auto upNewSound{ std::unique_ptr<FMOD::Sound, FMODSoundDeleter>(rawSound) };
 
-	// サウンドを shared_ptr に変換してキャッシュに格納
-	std::shared_ptr<FMOD::Sound> spSound(newSound.release(), FMODSoundDeleter());
-	m_soundCache[filePath] = spSound;
+	// shared_ptr に変換してキャッシュに格納
+	auto spSound{ std::shared_ptr<FMOD::Sound>(upNewSound.release(), FMODSoundDeleter()) };
+	m_soundController[filePath] = { spSound, nullptr }; 
+
 	return spSound;
 }
 
-void FlAudioManager::Play(const std::string& filePath)
+void FlAudioManager::Play()
 {
-	std::shared_ptr<FMOD::Sound> sound = LoadSound(filePath);
-	FMOD::Channel* channel = nullptr;
-	m_upSystem->playSound(sound.get(), nullptr, false, &channel);
+	auto it{ m_soundController.begin() };
+
+	while (it != m_soundController.end())
+	{
+		// サウンドをロードし、存在を保証
+		if (!LoadSound(it->first)) return;
+
+		// m_soundController からサウンドとチャンネルを取得
+		auto& soundChannelPair{ m_soundController[it->first] };
+		auto& spSound         { soundChannelPair.first };
+		auto& upChannel       { soundChannelPair.second };
+
+		auto mode{ FMOD_MODE{} };
+		upChannel->getMode(&mode);
+
+		if (spSound)
+		{
+			auto rawChannel{ static_cast<FMOD::Channel*>(nullptr) };
+			m_upSystem->playSound(spSound.get(), nullptr, false, &rawChannel);
+
+			if (rawChannel) rawChannel->setMode(mode);
+
+			upChannel.reset(rawChannel);
+		}
+
+		++it;
+	}
+}
+
+void FlAudioManager::Play(const std::string& filePath, const bool isLoop)
+{
+	// サウンドをロードし、存在を保証
+	if (!LoadSound(filePath)) return;
+
+	// m_soundController からサウンドとチャンネルを取得
+	auto& soundChannelPair{ m_soundController[filePath] };
+	auto& spSound         { soundChannelPair.first };
+	auto& upChannel       { soundChannelPair.second };
+
+	if (spSound)
+	{
+		auto rawChannel{ static_cast<FMOD::Channel*>(nullptr) };
+		m_upSystem->playSound(spSound.get(), nullptr, false, &rawChannel);
+
+		if (rawChannel && isLoop) rawChannel->setMode(FMOD_LOOP_NORMAL);
+
+		upChannel.reset(rawChannel);
+	}
+}
+
+const bool FlAudioManager::IsPlay(const std::string& filePath)
+{
+	auto it{ m_soundController.find(filePath) };
+	auto is{ false };
+	if (it != m_soundController.end() && it->second.second) it->second.second->isPlaying(&is);
+	return is;
+}
+
+void FlAudioManager::Stop()
+{
+	auto it{ m_soundController.begin() };
+	while (it != m_soundController.end())
+	{
+		if (it->second.second) (*it).second.second->stop();
+		++it;
+	}
+}
+
+void FlAudioManager::Stop(const std::string& filePath)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) it->second.second->stop();
+}
+
+void FlAudioManager::Set3DSoundSettings(const FMOD_VECTOR& position, const FMOD_VECTOR& velocity)
+{
+	auto it{ m_soundController.begin() };
+	while (it != m_soundController.end())
+	{
+		if (it->second.second) (*it).second.second->set3DAttributes(&position, &velocity);
+		++it;
+	}
+}
+
+void FlAudioManager::Set3DSoundSettings(const std::string& filePath, const FMOD_VECTOR& position, const FMOD_VECTOR& velocity)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) it->second.second->set3DAttributes(&position, &velocity);
+}
+
+void FlAudioManager::SetVolume(float volume)
+{
+	auto it{ m_soundController.begin() };
+	while (it != m_soundController.end())
+	{
+		if (it->second.second) (*it).second.second->setVolume(volume);
+		++it;
+	}
+}
+
+void FlAudioManager::SetVolume(const std::string& filePath, float volume)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) it->second.second->setVolume(volume);
+}
+
+void FlAudioManager::SetPitch(float pitch)
+{
+	auto it{ m_soundController.begin() };
+	while (it != m_soundController.end())
+	{
+		if (it->second.second) (*it).second.second->setPitch(pitch);
+		++it;
+	}
+}
+
+void FlAudioManager::SetPitch(const std::string& filePath, float pitch)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) it->second.second->setPitch(pitch);
+}
+
+void FlAudioManager::Pause()
+{
+	auto it{ m_soundController.begin() };
+	while (it != m_soundController.end())
+	{
+		if (it->second.second) (*it).second.second->setPaused(true);
+		++it;
+	}
+}
+
+void FlAudioManager::Pause(const std::string& filePath)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) it->second.second->setPaused(true);
+}
+
+void FlAudioManager::Resume()
+{
+	auto it{ m_soundController.begin() };
+	while (it != m_soundController.end())
+	{
+		if (it->second.second) (*it).second.second->setPaused(false);
+		++it;
+	}
+}
+
+void FlAudioManager::Resume(const std::string& filePath)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) it->second.second->setPaused(false);
+}
+
+void FlAudioManager::SetReverb(float reverbLevel)
+{
+	auto it{ m_soundController.begin() };
+	auto rawReverbDSP{ static_cast<FMOD::DSP*>(nullptr) };
+
+	m_upSystem->createDSPByType(FMOD_DSP_TYPE_SFXREVERB, &rawReverbDSP);
+	rawReverbDSP->setParameterFloat(FMOD_DSP_SFXREVERB_WETLEVEL, reverbLevel);
+
+	while (it != m_soundController.end())
+	{
+		if(it->second.second) (*it).second.second->addDSP(0, rawReverbDSP);
+		++it;
+	}
+	rawReverbDSP->release();
+}
+
+void FlAudioManager::SetReverb(const std::string& filePath, float reverbLevel)
+{
+	auto it{ m_soundController.find(filePath) };
+	if (it != m_soundController.end() && it->second.second) 
+	{
+		auto rawReverbDSP{ static_cast<FMOD::DSP*>(nullptr) };
+		m_upSystem->createDSPByType(FMOD_DSP_TYPE_SFXREVERB, &rawReverbDSP);
+		rawReverbDSP->setParameterFloat(FMOD_DSP_SFXREVERB_WETLEVEL, reverbLevel);
+		it->second.second->addDSP(0, rawReverbDSP);
+		rawReverbDSP->release();
+	}
 }
 
 void FlAudioManager::Init()
 {
-	FMOD::System* rawSystem = nullptr;
+	auto rawSystem{ static_cast<FMOD::System*>(nullptr) };
+
 	FMOD::System_Create(&rawSystem);
-	m_upSystem.reset(rawSystem); // unique_ptr に設定
+	m_upSystem.reset(rawSystem);
 	m_upSystem->init(512, FMOD_INIT_NORMAL, nullptr);
 }
 
--- 3DGame(2-3)/Src/Framework/Audio/FlAudioManager.h
+++ 3DGame(2-3)/Src/Framework/Audio/FlAudioManager.h
@@ -1,32 +1,38 @@
 ﻿#pragma once
 
-// FMOD のカスタムデリータを定義
-struct FMODSystemDeleter {
-	void operator()(FMOD::System* system) const {
-		if (system) {
-			system->release();
-		}
-	}
-};
-
-struct FMODSoundDeleter {
-	void operator()(FMOD::Sound* sound) const {
-		if (sound) {
-			sound->release();
-		}
-	}
-};
-
 class FlAudioManager {
 public:
 	FlAudioManager() { Init(); }
 	~FlAudioManager() = default;
 
 	// サウンドを読み込む関数
-	std::shared_ptr<FMOD::Sound> LoadSound(const std::string& filePath);
+	const std::shared_ptr<FMOD::Sound> LoadSound(const std::string& filePath);
 
 	// サウンドを再生する関数
-	void Play(const std::string& filePath);
+	void Play();
+	void Play(const std::string& filePath, const bool isLoop = false);
+	const bool IsPlay(const std::string& filePath);
+
+	void Stop();
+	void Stop(const std::string& filePath);
+
+	void Set3DSoundSettings(const FMOD_VECTOR& position, const FMOD_VECTOR& velocity);
+	void Set3DSoundSettings(const std::string& filePath, const FMOD_VECTOR& position, const FMOD_VECTOR& velocity);
+
+	void SetVolume(float volume);
+	void SetVolume(const std::string& filePath, float volume);
+
+	void SetPitch(float pitch);
+	void SetPitch(const std::string& filePath, float pitch);
+
+	void Pause();
+	void Pause(const std::string& filePath);
+
+	void Resume();
+	void Resume(const std::string& filePath);
+
+	void SetReverb(float reverbLevel);
+	void SetReverb(const std::string& filePath, float reverbLevel);
 
 	// 初期化関数
 	void Init();
@@ -38,10 +44,27 @@ public:
 	FlAudioManager& operator=(const FlAudioManager&) = delete;
 private:
 
+	// FMOD のカスタムデリータを定義
+	struct FMODSystemDeleter {
+		void operator()(FMOD::System* system) const {
+			if (system) system->release();
+		}
+	};
+
+	struct FMODSoundDeleter {
+		void operator()(FMOD::Sound* sound) const {
+			if (sound) sound->release();
+		}
+	};
+
+	struct FMODChannelDeleter {
+		void operator()(FMOD::Channel* channel) const {
+			if (channel) channel->stop();
+		}
+	};
+
 	// FMOD::System を unique_ptr で管理
 	std::unique_ptr<FMOD::System, FMODSystemDeleter> m_upSystem;
-	// サウンドキャッシュ: ファイルパスをキーにした shared_ptr
-	std::unordered_map<std::string, std::shared_ptr<FMOD::Sound>> m_soundCache;
 
-	std::vector<std::shared_ptr<FMOD::Channel>> m_channels;
+	std::unordered_map<std::string, std::pair<std::shared_ptr<FMOD::Sound>, std::unique_ptr<FMOD::Channel, FMODChannelDeleter>>> m_soundController;
 };
